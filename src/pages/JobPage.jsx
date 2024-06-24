72f507d866f8ea8cce50858623f65304d923a6c1
luckylaw01 <munyakalawrence01@gmail.com>
2024-06-24 11:16:24 +0300

    New changes: View sing job

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
 
 const JobPage = () => {
+  
+  const { id } = useParams(); // Call useParams as a function
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        console.log(data);
+        setJob(data);
+      } catch (error) { // Add error parameter here
+        console.log("error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJob(); // Call the fetchJob function inside useEffect
+  }, [id]); // Add id as a dependency
+  
   return (
-    <div>
-      JobPage
-    </div>
-  )
+    loading ? <Spinner /> : <h1>{job.title}</h1>
+  );
 }
 
-export default JobPage
+export default JobPage;
