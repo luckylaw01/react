87e1b365026473e0843941a589b63430c39b6036
luckylaw01 <munyakalawrence01@gmail.com>
2024-06-26 15:17:34 +0300

    New changes - add project

--- src/App.jsx
+++ src/App.jsx
@@ -41,21 +41,29 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
 
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  
-  )
-)
 const App = () => {
+  const addJob = (newJob) => {
+    console.log(newJob);
+  }
+  
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+  
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobsSubmit={addJob}/>} />
+        <Route path='/jobs/:id' element={<JobPage />} loader = {jobLoader} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    
+    )
+  );
   return <RouterProvider router={router}/>
 }
 
